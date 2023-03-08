7431e9721106a5fbd1f059f8e9b58d2f7791109e
Logical_Mind <nanocinetico@gmail.com>
2023-03-07 21:09:35 -0300

    Mi_primer_Commit

---
+++ Egg_primer_proyecto/src/main/java/com/mycompany/egg_primer_proyecto/Egg_primer_proyecto.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.egg_primer_proyecto;
+
+/**
+ *
+ * @author hp
+ */
+public class Egg_primer_proyecto {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
